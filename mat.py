ebd7da2e3068a680e7392c52975775e7b36bf954
arathyrajendran <arathyrajendran148@gmail.com>
2025-08-21 11:47:38 +0530

    Add files via upload

---
+++ mat.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+
+def create_matrix(mc):
+    print("\nArray" + str(mc) + " Elements:")
+    array_1 = map(int, input().split())
+    array_1 = np.array(list(array_1))
+    
+    print("\nArray" + str(mc) + " Row and Column:")
+    row, column = map(int, input().split())
+
+    if len(array_1) != (row * column):
+        print("\nRow and column size not matching with total elements!! Retry")
+        return create_matrix(mc)
+    
+    array_1 = array_1.reshape(row, column)
+    print("\nARRAY" + str(mc))
+    print(array_1)
+    return array_1
+
+arr1 = create_matrix(1)
+arr2 = create_matrix(2)
+
+if arr1.shape == arr2.shape:
+    print("\nDot Product")
+    print(np.dot(arr1, arr2.T)) 
+else:
+    print("\nDimensions not matching!")
